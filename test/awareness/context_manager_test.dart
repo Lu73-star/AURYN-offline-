cedf4659329172e4f1fa740b8ca2bc4cdc9d387d
Lu73-star <992097617luciano@gmail.com>
2025-12-05 15:57:49 -0300

    Adiciona teste unitário básico para ContextManager em test/awareness/context_manager_test.dart (Flutter padrão).

---
+++ test/awareness/context_manager_test.dart
@@ -0,0 +1,24 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:auryn_core/awareness/context_manager.dart';
+
+void main() {
+  group('ContextManager', () {
+    test('Atualiza e retorna contexto correto', () {
+      final manager = ContextManagerTestDouble();
+      manager.updateContext({'state': 'init'});
+      expect(manager.getCurrentContext()['state'], equals('init'));
+    });
+  });
+}
+
+class ContextManagerTestDouble implements ContextManager {
+  Map<String, dynamic> ctx = {};
+
+  @override
+  Map<String, dynamic> getCurrentContext() => ctx;
+
+  @override
+  void updateContext(Map<String, dynamic> changes) {
+    ctx.addAll(changes);
+  }
+}
