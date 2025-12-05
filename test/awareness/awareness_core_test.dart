6475c50c4250eedb3b59074ad78162e13b3f3432
Lu73-star <992097617luciano@gmail.com>
2025-12-05 15:40:44 -0300

    Adiciona teste básico de inicialização para AwarenessCore conforme padrão flutter_test

---
+++ test/awareness/awareness_core_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:auryn_core/awareness/awareness_core.dart';
+
+void main() {
+  group('AwarenessCore', () {
+    test('initialize não lança exceção', () {
+      final core = AwarenessCoreTestDouble();
+      expect(() => core.initialize(), returnsNormally);
+    });
+  });
+}
+
+class AwarenessCoreTestDouble implements AwarenessCore {
+  @override
+  void initialize() {}
+  @override
+  void update() {}
+}
